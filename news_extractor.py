21909497d798cae1b90abe99fc4dca9923e7768e
ShubhamSetia <shubham.setia12@gmail.com>
2017-06-19 15:36:44 +0530

    created function to store result in file

--- news_extractor.py
+++ news_extractor.py
@@ -1,5 +1,5 @@
 import newspaper
-
+import csv
 
 class newsExtractor(object):
 	"""docstring for newsExtractor"""
@@ -7,8 +7,9 @@ class newsExtractor(object):
 		self.url = url
 		self.paper = newspaper.build(url,memoize_articles=False)
 		self.result=[["Title","Article"]]
+		print("initialized successfully")
 
-	def extract_article():
+	def extract_article(self):
 		length = self.paper.size()
 		
 		for i in range(length):
@@ -18,5 +19,17 @@ class newsExtractor(object):
 			articleText=article.text
 			articleTitle=article.title
 			self.result.append([articleTitle,articleText])
+		print("extracted successfully")
 
-	
+	def store_result(self,filename):
+		with open(filename,'w') as f:
+			w= csv.writer(f)
+			for row in self.result:
+				w.writerow(row)
+
+
+
+hindustan_times = newsExtractor("http://www.hindustantimes.com/")
+hindustan_times.extract_article()
+hindustan_times.store_result("hindustan_times.csv")
+
