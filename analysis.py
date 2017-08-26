e32d5a7ae41aaffb5a5293e8f9fcc689f57f476a
ShubhamSetia <shubham.setia12@gmail.com>
2017-08-26 19:21:43 +0530

    Exception handling and storing data in organized way

--- analysis.py
+++ analysis.py
@@ -4,7 +4,11 @@ from textblob import TextBlob
 import statistics
 import matplotlib.pyplot as plt
 
-filename = ['hindu_5_Aug.csv','ht_5_Aug.csv','dh_5_Aug.csv','toi_5_Aug.csv','tribune_5_Aug.csv']
+newspaperNames = ['ht','hindu','toi','tribune','ie','et','dh']
+date = '16'
+filename=[]
+for name in newspaperNames:
+	filename.append('./data/'+name+date+'.csv')
 pol = []
 sub = []
 for x in filename:
@@ -26,19 +30,21 @@ for x in filename:
 		#print(analysis.sentiment)
 		a=analysis.sentiment.polarity
 		b=analysis.sentiment.subjectivity
-		print(analysis.sentiment)
+		#print(analysis.sentiment)
 		t1.append(a)
 		t2.append(b)
 	pol.append(statistics.mean(t1))
 	sub.append(statistics.mean(t2))
-	print(x+' pol: '+str(statistics.mean(t1))+' sub: '+ str(statistics.mean(t2)))
+	#print(x+' pol: '+str(statistics.mean(t1))+' sub: '+ str(statistics.mean(t2)))
 	#print(statistics.mean(t1),statistics.mean(t2))
 plt.plot(pol[0],sub[0],'ro')
 plt.plot(pol[1],sub[1],'bo')
 plt.plot(pol[2],sub[2],'go')
 plt.plot(pol[3],sub[3],'yo')
 plt.plot(pol[4],sub[4],'mo')
+plt.plot(pol[5],sub[5],'co')
+plt.plot(pol[6],sub[6],'ko')
 plt.ylabel('subjectivity')
 plt.xlabel('polarity')
 plt.show()
-
+	
